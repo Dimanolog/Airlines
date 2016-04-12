2e35127d5ac53843347bd473f2739bacd588325c
unknown <Dimanolog86@gmail.com>
2016-04-12 17:02:35 +0300

    no message

---
+++ DBModel/src/main/java/by/trainings/java8/year2016/dzshnipko/airlines/AbstractModel.java
@@ -0,0 +1,15 @@
+package by.trainings.java8.year2016.dzshnipko.airlines;
+
+public class AbstractModel {
+
+	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+}
